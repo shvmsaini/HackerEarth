4561772c738940d2436f29aed127bd6955d0cd5f
Shivam saini <noddycheeks@gmail.com>
2020-05-26 22:55:29 +0530

    Create BinaryQueries.c

---
+++ Data Structures/1-D Array/BinaryQueries.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <math.h>
+ 
+int main(){
+long size;
+long query;
+int A[1000000];
+long L,R,X,q;
+ 
+scanf("%ld",&size);
+scanf("%ld",&query);
+for (int i =0; i < size; i++){
+	scanf("%d",&A[i]);
+}
+while(query--){
+	scanf("%ld",&q);
+	if (q==1){
+		scanf("%ld",&X);
+		
+			if (A[X-1]==0){
+				A[X-1]=1;
+			} 
+			else{
+				A[X-1]=0;
+			}
+	
+		
+	}
+	
+	else if(q==0){
+		scanf("%ld %ld",&L,&R);
+		
+		if (A[R-1]&1) { //if the last digit in a binary number is 0 then it is even otherwise odd
+				printf("ODD\n");
+			}
+		else {
+				printf("EVEN\n");
+		}
+		
+ 
+	}
+}
+ 
+}
